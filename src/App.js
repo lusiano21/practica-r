8db19882bf965df4bab7b20953b3f235f15512ca
lusiano <lusianobonifacio6@gmail.com>
2022-11-23 11:45:51 -0300

    Proyecto1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ItemisContainer from "./components/itemisContainer";
+import Navbar from "./components/Navbar";
+const App = () => {
+  return(
+    <div>
+    <Navbar />
+    <h1>Heladria</h1>
+    <h2>Bienvenido</h2>
+    <ItemisContainer h1="Chocolate" h2="Vainilla" nombre="Juan" />
+    </div> 
+  )
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return(
+        <nav className="navbar bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <a className="navbar-brand" href="#">Categorias</a>
+          <a className="navbar-brand" href="#">Armar pedido</a>
+          <a className="navbar-brand" href="#">Quienes somos</a>
+        </div>
+      </nav>
+    )
+  }
+
+  export default Navbar;  
